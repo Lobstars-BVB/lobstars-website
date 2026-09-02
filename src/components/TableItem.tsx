0a7c8a0d4c973d2083d0adaf382d5331ec35c43c
rnegash <rnegash@gmail.com>
2026-09-02 14:12:03 +0200

    refactor

--- src/components/TableItem.tsx
+++ src/components/TableItem.tsx
@@ -1,7 +1,16 @@
 import type { ReactNode } from "react";
 
-export const TableItem = ({ children }: { children: ReactNode }) => (
-  <td className="max-w-[100px] overflow-hidden text-ellipsis py-4 pr-2">
+export const TableItem = ({
+  children,
+  title,
+}: {
+  children: ReactNode;
+  title?: string;
+}) => (
+  <td
+    className="max-w-[100px] overflow-hidden text-ellipsis py-4 pr-2"
+    title={title}
+  >
     {children}
   </td>
 );
